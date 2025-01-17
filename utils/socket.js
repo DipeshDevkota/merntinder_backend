da12a7b924d57bbcb4dac9d03c1e2776bbf96439
federallol78 <devkotadipesh12@gmail.com>
2025-01-17 21:57:24 +0545

    Issues arriving on socket

--- utils/socket.js
+++ utils/socket.js
@@ -3,41 +3,35 @@ const socket = require("socket.io");
 const initializeSocket = (server) => {
   const io = socket(server, {
     cors: {
-      origin:" http://localhost:5173",
+      origin: "http://localhost:5173", // Make sure this is correct
     },
   });
 
   io.on("connection", (socket) => {
-    //now you can perform different events
-    socket.on("joinChat", ({id,firstName,userId}) => {
-        console.log("UserId is",userId)
-        console.log("TargetUserId is",id)
-
-         const roomId= [userId,id].sort().join("_")
-         console.log("RoomId is ",roomId);
-         socket.join(roomId)
-        console.log(firstName+"joined room",userId)
-
-
-
+    // Handling the joinChat event
+    socket.on("joinChat", ({ id, firstName, userId }) => {
+      console.log("UserId is", userId);
+      console.log("TargetUserId is", id);
+2
+      const roomId = [userId, id].sort().join("_");
+      console.log("RoomId is ", roomId);
+      socket.join(roomId);
+      console.log(firstName + " joined room", userId);
     });
 
-    socket.on("sendMessage", ({id,firstName,newMessage,userId}) => {
-      console.log("Socketonsendmessagecalled")
-      console.log("Message that is sent to the cliens id ",id)
-      console.log("Loggedin Users Id",userId)
-      console.log("Loggedin Users firstName",firstName)
-      console.log("Message while sending the message",newMessage)
+    // Handling the sendMessage event
+    socket.on("sendMessage", ({ id, firstName, text, userId }) => {
+      console.log("Socket on sendMessage called");
+      const roomId = [userId, id].sort().join("_");
+      console.log("RoomId is ", roomId);
 
-      const roomId= [userId,id].sort().join("_");
-         console.log("RoomId is ",roomId);
-      
-         io.to(roomId).emit("messageReceived",{firstName,text})
-      
-
+      // Emit the message to the room
+      io.to(roomId).emit("messageReceived", { firstName, text });
     });
 
-    socket.on("disconnect", () => {});
+    socket.on("disconnect", () => {
+      // Handle disconnections if needed
+    });
   });
 };
 

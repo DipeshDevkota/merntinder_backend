9112baa9dd7c2849008cee4376f422524866ac0b
federallol78 <devkotadipesh12@gmail.com>
2025-01-21 16:21:13 +0545

    rate limiting

--- middlewares/ratelimit.js
+++ middlewares/ratelimit.js
@@ -1,12 +1,17 @@
-const rateLimit= require('express-rate-limit');
-const loginRateLimiter= rateLimit({
-    windowMs: 5 * 60 * 1000, //5min in milliseconds
-    max:5,
-    message:'Login error, you have reached maximum retries.Please try again after 5 minutes',
-    statusCode: 429,
-    headers:true,
+const rateLimit = require('express-rate-limit');
+const RateLimitMongo = require('rate-limit-mongo');
+
+const loginRateLimiter = rateLimit({
+    windowMs: 1 * 60 * 1000, // 5 minutes in milliseconds
+    max: 5, // Limit each IP to 5 login requests per windowMs
+    message: 'Too many login attempts. Please try again after 5 minutes.',
+    statusCode: 429, // HTTP status code to return when rate-limited
+    headers: true, // Include rate limit headers in the response
+    store: new RateLimitMongo({
+        uri: process.env.MONGO_URI, // MongoDB connection string
+        collectionName: 'rateLimit', // Name of the MongoDB collection
+        expireTimeMs: 5 * 60 * 1000, // Expiration time for records
+    }),
 });
 
-
-
-module.exports={loginRateLimiter}
+module.exports = { loginRateLimiter };
